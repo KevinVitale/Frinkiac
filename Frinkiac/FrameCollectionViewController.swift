3b7eb4d34eb8a0634140ca6fae60166021ec1dd5
Kevin Vitale <kevinvitale@gmail.com>
2016-09-28 12:26:30 -0400

    [Frinkiac] 'FrameCollection' now subclasses 'UICollectionViewController'

--- Frinkiac/FrameCollectionViewController.swift
+++ Frinkiac/FrameCollectionViewController.swift
@@ -3,25 +3,31 @@ import UIKit
 
 // MARK: - Frame Collection View Controller -
 //------------------------------------------------------------------------------
-public final class FrameCollectionViewController: UIViewController, FrameImageDelegate {
+public final class FrameCollectionViewController: UICollectionViewController, FrameImageDelegate {
     // MARK: - Public -
     //--------------------------------------------------------------------------
     public var images: [FrameImage] = [] {
         didSet {
             DispatchQueue.main.async { [weak self] in
-                self?.collectionView.reloadData()
+                self?.collectionView?.reloadData()
             }
         }
     }
 
-    // MARK: - Outlets -
-    //--------------------------------------------------------------------------
-    @IBOutlet var collectionView: UICollectionView! = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-
     // MARK: - Computed -
     //--------------------------------------------------------------------------
     var flowLayout: UICollectionViewFlowLayout {
-        return collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        return collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+    }
+
+    // MARK: - Initialization -
+    //--------------------------------------------------------------------------
+    public required init?(coder aDecoder: NSCoder) { super.init(coder: aDecoder) }
+    public override init(collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(collectionViewLayout: layout)
+    }
+    public convenience init() {
+        self.init(collectionViewLayout: UICollectionViewFlowLayout())
     }
 
     // MARK: - View Lifecycle -
@@ -29,38 +35,38 @@ public final class FrameCollectionViewController: UIViewController, FrameImageDe
     public override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Root View
-        //----------------------------------------------------------------------
-        view.addSubview(collectionView)
-        collectionView.frame = view.frame
-        collectionView.backgroundColor = .simpsonsYellow
-
         // Collection View
-        //----------------------------------------------------------------------
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.autoresizingMask = [
-              .flexibleWidth
-            , .flexibleHeight
-        ]
-
-        // Collection Layout
-        //----------------------------------------------------------------------
-        let inset: CGFloat = 8.0
-        let spacing: CGFloat = 8.0
-        flowLayout.sectionInset = UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
-        flowLayout.minimumInteritemSpacing = spacing
-        flowLayout.minimumLineSpacing = spacing
+        //--------------------------------------------------------------------------
+        collectionView?.backgroundColor = .simpsonsYellow
+        collectionView?.alwaysBounceHorizontal = false
 
         // Cell Types
         //----------------------------------------------------------------------
-        collectionView.register(FrameImageCell.self, forCellWithReuseIdentifier: FrameImageCell.cellIdentifier)
+        collectionView?.register(FrameImageCell.self, forCellWithReuseIdentifier: FrameImageCell.cellIdentifier)
+
+        // Collection Layout
+        //----------------------------------------------------------------------
+        let inset: CGFloat = 4.0
+        let spacing: CGFloat = 4.0
+        flowLayout.sectionInset = UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+        flowLayout.minimumInteritemSpacing = spacing
+        flowLayout.minimumLineSpacing = spacing
+    }
+
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        // Layout Guide Insets
+        //----------------------------------------------------------------------
+        let layoutGuideInsets = UIEdgeInsets(top: topLayoutGuide.length, left: 0.0, bottom: bottomLayoutGuide.length, right: 0.0)
+        collectionView?.contentInset = layoutGuideInsets
+        collectionView?.scrollIndicatorInsets = layoutGuideInsets
     }
 
     // MARK: - Dequeue Cell -
     //--------------------------------------------------------------------------
     fileprivate func dequeue(frameCellAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FrameImageCell.cellIdentifier, for: indexPath) as! FrameImageCell
+        let cell = collectionView?.dequeueReusableCell(withReuseIdentifier: FrameImageCell.cellIdentifier, for: indexPath) as! FrameImageCell
 
         let image = images[indexPath.row].image
         cell.imageView.image = image
@@ -71,23 +77,23 @@ public final class FrameCollectionViewController: UIViewController, FrameImageDe
     //--------------------------------------------------------------------------
     public func frame(_ frame: FrameImage, didUpdate image: UIImage) {
         if let index = images.index(where: { $0 == frame }) {
-            collectionView.reloadItems(at: [IndexPath(row: index, section: 0)])
+            collectionView?.reloadItems(at: [IndexPath(row: index, section: 0)])
         }
     }
 }
 
 // MARK: - Extension, Data Source -
 //------------------------------------------------------------------------------
-extension FrameCollectionViewController: UICollectionViewDataSource {
-    public func numberOfSections(in collectionView: UICollectionView) -> Int {
+extension FrameCollectionViewController {
+    public override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
 
-    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
 
-    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    public override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         return dequeue(frameCellAt: indexPath)
     }
 }
