172e00653190e2cb5d2e736ee5b3edafd5bc3e14
Kevin Vitale <kevinvitale@gmail.com>
2016-09-29 17:26:15 -0400

    [Misc] moves ‘UIColor’ extensions into ‘Aliases’

--- Frinkiac/FrameCollectionViewController.swift
+++ Frinkiac/FrameCollectionViewController.swift
@@ -121,31 +121,6 @@ extension FrameCollectionViewController: UICollectionViewDelegateFlowLayout {
         return CGSize(width: itemWidth, height: itemWidth)
     }
 }
-    
-// MARK: - Extension, Simpsons Yellow -
-//------------------------------------------------------------------------------
-extension UIColor {
-    public class var simpsonsYellow: UIColor {
-        return UIColor(red: 1.0, green: (217.0/255.0), blue: (15.0/255.0), alpha: 1.0)
-    }
-
-    public class var simpsonsBlue: UIColor {
-        return UIColor(red: (23.0/255.0), green: (145.0/255.0), blue: 1.0, alpha: 1.0)
-    }
-
-    public class var simpsonsPaleBlue: UIColor {
-        return UIColor(red: (112.0/255.0), green: (209.0/255.0), blue: 1.0, alpha: 1.0)
-    }
-
-    public class var simpsonsPastelGreen: UIColor {
-        return UIColor(red: (209.0/255.0), green: 1.0, blue: (135.0/255.0), alpha: 1.0)
-    }
-
-    public class var simpsonsFleshy: UIColor {
-        return UIColor(red: (209.0/255.0), green: (178.0/255.0), blue: (112.0/255.0), alpha: 1.0)
-    }
-}
-
 
 // MARK: - Frame Collection Delegate -
 //------------------------------------------------------------------------------
