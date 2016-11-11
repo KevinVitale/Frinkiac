79f1c4ca37d6d671705055c0cb309835e2d79dac
Kevin Vitale <kevinvitale@gmail.com>
2016-11-11 15:14:44 -0500

    [AppDelegate] breaks up functionality into extension groups

--- FrinkiacApp/Frinkiac-macOS/AppDelegate.swift
+++ FrinkiacApp/Frinkiac-macOS/AppDelegate.swift
@@ -8,24 +8,7 @@ import Frinkiac
 class AppDelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate {
     // MARK: - Private -
     //--------------------------------------------------------------------------
-    private let memeGenerator = Frinkiac()
-    
-    // MARK: - Outlets -
-    //--------------------------------------------------------------------------
-    @IBOutlet weak var window: NSWindow!
-    @IBOutlet weak var imageView: NSImageView!
-    @IBOutlet weak var searchTextField: NSSearchField!
-    @IBOutlet weak var textField: NSTextField!
-
-    // MARK: - Private -
-    //--------------------------------------------------------------------------
-    private func setImage(_ image: ImageType?, text: String = "") {
-        DispatchQueue.main.async { [weak self] in
-            self?.imageView.image = image
-            self?.textField.stringValue = text
-        }
-    }
-    private var frameImage: FrameImage<Frinkiac>? = nil {
+    fileprivate var frameImage: FrameImage<Frinkiac>? = nil {
         didSet {
             frameImage?.caption { [weak self] in
                 if let image = try? $0()?.image {
@@ -34,10 +17,20 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate {
             }
         }
     }
-    private var searchProvider: FrameSearchProvider<Frinkiac>!
+    fileprivate let memeGenerator = Frinkiac()
+    fileprivate var searchProvider: FrameSearchProvider<Frinkiac>!
 
-    // MARK: - Initialization -
+    // MARK: - Outlets -
     //--------------------------------------------------------------------------
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var searchTextField: NSSearchField!
+    @IBOutlet weak var textField: NSTextField!
+}
+
+// MARK: - Extension, Initialization -
+//------------------------------------------------------------------------------
+extension AppDelegate {
     override func awakeFromNib() {
         super.awakeFromNib()
         //----------------------------------------------------------------------
@@ -45,29 +38,11 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate {
             self?.frameImage = $0.first
         }
     }
+}
 
-    // MARK: - Control Callbacks -
-    //--------------------------------------------------------------------------
-    override func controlTextDidChange(_ obj: Notification) {
-        switch obj.object {
-        case let searchField as NSSearchField where searchField.isEqual(searchTextField):
-            search(obj.object)
-        default:
-            setImage(frameImage?.image, text: textField.stringValue)
-        }
-    }
-
-    /// https://developer.apple.com/library/content/qa/qa1454/_index.html
-    func control(_ control: NSControl, textView: NSTextView, doCommandBy commandSelector: Selector) -> Bool {
-        if commandSelector == #selector(NSResponder.insertNewline(_:)) {
-            textView.insertNewlineIgnoringFieldEditor(self)
-            return true
-        }
-        return false
-    }
-
-    // MARK: - Actions -
-    //--------------------------------------------------------------------------
+// MARK: - Extension, Actions -
+//------------------------------------------------------------------------------
+extension AppDelegate {
     @IBAction func search(_ sender: Any?) {
         searchProvider.find(searchTextField.stringValue)
     }
@@ -75,7 +50,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate {
     @IBAction func random(_ sender: Any?) {
         searchProvider.random { [weak self] in
             if let caption = try? $0().caption
-                , let memeGenerator = self?.memeGenerator {
+             , let memeGenerator = self?.memeGenerator {
                 self?.frameImage = FrameImage(memeGenerator, frame: caption.frame)
             }
         }
@@ -83,5 +58,42 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate {
     
     @IBAction func update(_ sender: Any?) {
         setImage(frameImage?.image, text: textField.stringValue)
+    }
+}
+
+// MARK: - Extension, Set Image -
+//------------------------------------------------------------------------------
+extension AppDelegate {
+    // MARK: - Private -
+    //--------------------------------------------------------------------------
+    fileprivate func setImage(_ image: ImageType?, text: String = "") {
+        DispatchQueue.main.async { [weak self] in
+            self?.imageView.image = image
+            self?.textField.stringValue = text
+        }
+    }
+}
+
+// MARK: - Extension, Control Callbacks -
+// https://developer.apple.com/library/content/qa/qa1454/_index.html
+//------------------------------------------------------------------------------
+extension AppDelegate {
+    /// Sent when the text in the receiving control changes.
+    override func controlTextDidChange(_ obj: Notification) {
+        switch obj.object {
+        case let searchField as NSSearchField where searchField.isEqual(searchTextField):
+            search(obj.object)
+        case let textField as NSTextField where textField.isEqual(textField):
+            setImage(frameImage?.image, text: textField.stringValue)
+        default: ()
+        }
+    }
+
+    func control(_ control: NSControl, textView: NSTextView, doCommandBy commandSelector: Selector) -> Bool {
+        if commandSelector == #selector(NSResponder.insertNewline(_:)) {
+            textView.insertNewlineIgnoringFieldEditor(self)
+            return true
+        }
+        return false
     }
 }
