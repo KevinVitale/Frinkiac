45e4da43444eec3b1ad70211e03b558e2311491a
Kevin Vitale <kevinvitale@gmail.com>
2016-11-11 13:57:23 -0500

    [ImageGenerator] moves API services into an extension on protocol

--- Frinkiac/ImageProvider.swift
+++ Frinkiac/ImageProvider.swift
@@ -13,6 +13,31 @@ extension ImageGenerator {
 // MARK: - Extension, API Services -
 //------------------------------------------------------------------------------
 extension ImageGenerator {
+    public func image(frame: Frame, text: MemeText? = nil, callback: @escaping Callback<(ImageType?, URLResponse?)>) -> URLSessionDownloadTask {
+        return download(endpoint: frame.imageLink, parameters: text?.query, callback: { closure in
+            callback {
+                let result = try closure()
+                let image = try result.0.image()
+                let response = result.1
+                return(image, response)
+            }
+        })
+    }
+    
+    public func gif(frame: Frame, duration: Int, text: MemeText? = nil, callback: @escaping Callback<(ImageType?, URLResponse?)>) -> URLSessionDownloadTask {
+        return download(endpoint: frame.gifLink(duration: duration), parameters: text?.query, callback: { closure in
+            callback {
+                let result = try closure()
+                let image = try result.0.image()
+                let response = result.1
+                return(image, response)
+            }
+        })
+    }
+
+    public func gif(start: Frame, end: Frame, text: MemeText? = nil, callback: @escaping Callback<(ImageType?, URLResponse?)>) -> URLSessionDownloadTask {
+        return gif(frame: start, duration: start.duration(between: end), callback: callback)
+    }
 }
 
 // MARK: - Image Provider -
@@ -44,35 +69,8 @@ public struct ImageProvider: ImageGenerator {
     }
 }
 
-// MARK: - Fetch
-extension ImageProvider {
-    public func image(frame: Frame, text: MemeText? = nil, callback: @escaping Callback<(ImageType?, URLResponse?)>) -> URLSessionDownloadTask {
-        return download(endpoint: frame.imageLink, parameters: text?.query, callback: { closure in
-            callback {
-                let result = try closure()
-                let image = try result.0.image()
-                let response = result.1
-                return(image, response)
-            }
-        })
-    }
-
-    public func gif(frame: Frame, duration: Int, text: MemeText? = nil, callback: @escaping Callback<(ImageType?, URLResponse?)>) -> URLSessionDownloadTask {
-        return download(endpoint: frame.gifLink(duration: duration), parameters: text?.query, callback: { closure in
-            callback {
-                let result = try closure()
-                let image = try result.0.image()
-                let response = result.1
-                return(image, response)
-            }
-        })
-    }
-
-    public func gif(start: Frame, end: Frame, text: MemeText? = nil, callback: @escaping Callback<(ImageType?, URLResponse?)>) -> URLSessionDownloadTask {
-        return gif(frame: start, duration: start.duration(between: end), callback: callback)
-    }
-}
-
+// MARK: - Extension, Image -
+//------------------------------------------------------------------------------
 extension URL {
     fileprivate func image() throws -> ImageType? {
         let data = try Data(contentsOf: self)
