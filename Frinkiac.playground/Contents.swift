11d6ee5185990058beb308db6f9365ce9f3bd697
Kevin Vitale <kevinvitale@gmail.com>
2016-11-15 20:45:17 -0500

    [Playground] simpler playground code

--- Frinkiac.playground/Contents.swift
+++ Frinkiac.playground/Contents.swift
@@ -1,4 +1,8 @@
+import Frinkiac
 import PlaygroundSupport
+
+public typealias MemeService = Frinkiac
+public let searchController = FrameSearchController<MemeService>()
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 PlaygroundPage.current.liveView = searchController
--- Frinkiac.playground/Sources/SearchController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-import Frinkiac
-
-public typealias MemeService = Frinkiac
-
-public let searchController = FrameSearchController<MemeService>()
-public let searchProvider = FrameSearchProvider<MemeService> {
-    searchController.images = $0
-}
